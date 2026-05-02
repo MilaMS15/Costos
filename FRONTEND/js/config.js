8665ebcdfbc30009d22e488b4f3f00148278488a
MilaMS15 <milams.15102005@gmail.com>
2026-05-01 22:51:08 -0500

    frontend

--- FRONTEND/js/config.js
+++ FRONTEND/js/config.js
@@ -1,7 +1,8 @@
 // Archivo de configuración global
 // Cambia esta URL cuando despliegues el backend en Render
 // Reemplaza "tu-backend-render" por el nombre que te dio Render
-const API_URL = 'https://tu-backend-render.onrender.com/api';
+const API_URL = 'https://costos-d12p.onrender.com/api';
+
 
 // Para desarrollo local, comenta la línea de arriba y descomenta la de abajo:
 // const API_URL = 'http://localhost:5000/api';
