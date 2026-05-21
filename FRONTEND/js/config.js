27501bfee6ffcc6a6f96652eb2cd0d859a789676
andersonxsx03-beep <andersonxsx03@gmail.com>
2026-05-21 07:25:12 -0500

    Update API_URL for production deployment

--- FRONTEND/js/config.js
+++ FRONTEND/js/config.js
@@ -1,7 +1,7 @@
 // Archivo de configuración global
 // Cambia esta URL cuando despliegues el backend en Render
 // Reemplaza "tu-backend-render" por el nombre que te dio Render
-const API_URL = 'https://costos-d12p.onrender.com/api';
+const API_URL = "https://projectcostosgrupo3.onrender.com";;
 //const API_URL = 'http://localhost:5000/api';
 // FRONTEND/js/config.js
 //const API_URL = '/api';
