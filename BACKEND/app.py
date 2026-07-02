74dac531a98bd16c6c8e3e60ff240cab7f473a8f
andersonxsx03-beep <andersonxsx03@gmail.com>
2026-07-02 17:21:57 -0500

    asda

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -1912,7 +1912,7 @@ register_menu5_routes(app)
 register_menu6_routes(app)
 register_menu7_routes(app)
 register_ia_routes(app)
-register_abc_routes(app)
+
 
 # ============================================
 # ENRUTAMIENTO DEL FRONTEND
